12ac36dca1262d2019d4cbe0fc9ddf820a3a8a15
shadow3aaa <shadow3aaa@qq.com>
2023-08-13 20:18:05 +0800

    Fix path

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -34,16 +34,16 @@ pub trait FileInterface: Sync + Send {
 // 目标symbol由shell调用readelf扫描，并且设置权限给surfaceflinger读取
 // 需要扫描两个symbol: 开始合成和合成结束
 pub unsafe fn target_symbol() -> Result<(*mut c_void, *mut c_void), Box<dyn Error>> {
-    let pre_path = Path::new("symbol_preComposition");
+    let pre_path = Path::new(HOOK_DIR).join("symbol_preComposition");
     let post_path = Path::new(HOOK_DIR).join("symbol_postComposition");
 
-    let pre_symbol = fs::read_to_string(pre_path)?;
+    let pre_symbol = fs::read_to_string(&pre_path)?;
     let post_symbol = fs::read_to_string(&post_path)?;
 
     let pre_symbol = CString::new(pre_symbol.trim())?;
     let post_symbol = CString::new(post_symbol.trim())?;
 
-    let _ = fs::remove_file(pre_path);
+    let _ = fs::remove_file(&pre_path);
     let _ = fs::remove_file(&post_path); // 读取完即可删除
 
     let pre_symbol = dobby::DobbySymbolResolver(ptr::null(), pre_symbol.as_ptr());
